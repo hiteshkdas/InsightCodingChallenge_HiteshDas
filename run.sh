3fc7abfe54ed19e0c594dde3b82a83916894d53f
hiteshkdas <hiteshkumardas@gmail.com>
2015-11-05 15:26:36 -0800

    Create run.sh
    Added run.sh which invokes "features.py" script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#########################################################################################################################################
+# Insight data engineering coding challenge
+# This code is developed by Hitesh Das. 
+# This code invokes the python code, "features.py" which handles both the features. This code was developed and executed on Cloud 9 IDE. 
+# Code submission date: Nov 5th, 2015.
+##########################################################################################################################################
+
+
+#!/bin/sh
+python features.py
